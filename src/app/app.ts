455628fe1b99418d23980ed11c4d34a708174593
beyzaBasaga <beyza.basaga@tnb.org.tr>
2026-02-23 18:21:27 +0300

    Signal examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
+import {Component, computed, effect, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mini-eticaret');
+  // SET örneği
+  countSet = signal(0);
+  incrementSet() {
+    this.countSet.set(this.countSet() + 1);
+  }
+
+  // UPDATE örneği
+  countUpdate = signal(0);
+  incrementUpdate() {
+    this.countUpdate.update(value => value + 1);
+  }
+
+  // COMPUTED örneği
+  price = signal(100);
+  taxRate = signal(0.18);
+  total = computed(() => this.price() * (1 + this.taxRate()));
+
+  // EFFECT örneği
+  name = signal('Angular');
+  nameInput = '';
+
+  constructor() {
+    effect(() => {
+      console.log('Name changed:', this.name());
+    });
+  }
+
+  changeName() {
+    this.name.set(this.nameInput);
+  }
 }
+
+
+
+
+
