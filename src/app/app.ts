dca3367d51d7a5fdd04725c730e9c028126d517c
10129342158 <ekrem.kocak@tnb.org.tr>
2026-02-23 16:35:47 +0300

    MyFirstComponent eklendi: bileşen (TS, HTML, boş CSS) ve spec dosyası oluşturuldu; App'e import edilip imports dizisine eklendi ve placeholder içeren app.html, <app-my-first-component> etiketiyle değiştirildi

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyFirstComponent } from './my-first-component/my-first-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('mini-eticaret');
