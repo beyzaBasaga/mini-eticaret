8cd11db59903d8adb0bcb65af9301ddb80094c10
beyzaBasaga <beyza.basaga@tnb.org.tr>
2026-02-23 18:48:00 +0300

    Directive examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ NgClass, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mini-eticaret');
+
+  // @if örneği
+  isLoggedIn = false;
+  toggleLogin() {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
+
+  // @for örneği
+  items = [
+    { id: 1, name: 'Laptop', price: 15000 },
+    { id: 2, name: 'Mouse', price: 300 },
+    { id: 3, name: 'Klavye', price: 700 }
+  ];
+
+  // @switch örneği
+  status: 'loading' | 'success' | 'error' | 'idle' = 'idle';
+  setStatus(newStatus: 'loading' | 'success' | 'error' | 'idle') {
+    this.status = newStatus;
+  }
+
+  //ngStyle örneği
+  isActive = true;
+
+  toggleActive()
+  {
+    this.isActive = !this.isActive;
+  }
 }
