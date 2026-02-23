72432da5ba56574f09d579590c7f43ec7d8444ad
beyzaBasaga <beyza.basaga@tnb.org.tr>
2026-02-23 17:52:10 +0300

    Data binding examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ FormsModule, CurrencyPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mini-eticaret');
+
+  userName: string = 'Angular';
+  fiyat: string = '150';
+  profileImage: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+  count: number = 0;
+  increase() {
+    this.count++;
+  }
+
+  userName2: string = 'Angular';
 }
